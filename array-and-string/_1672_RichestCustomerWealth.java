1ae4c98e520c1528f503e04b659961e2116e13d6
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2021-11-17 19:07:00 +0700

    Toi uu code: Mang 2 chieu - LC1672

--- array-and-string/_1672_RichestCustomerWealth.java
+++ array-and-string/_1672_RichestCustomerWealth.java
@@ -1,19 +1,30 @@
 public class _1672_RichestCustomerWealth {
     public static int maximumWealth(int[][] accounts) {
-        int soDong = accounts.length;
         int max = 0;
+        int soDong = accounts.length;
         int soCot = accounts[0].length;
-        int[] tongTaiSan = new int[soDong];
+        // int[] tongTaiSan = new int[soDong];
+        // for (int i = 0; i < soDong; i++) {
+        // int tongTaiSanI = 0;
+        // for (int j = 0; j < soCot; j++) {
+        // tongTaiSanI += accounts[i][j];
+        // }
+        // tongTaiSan[i] = tongTaiSanI;
+        // if (tongTaiSan[i] > max) {
+        // max = tongTaiSan[i];
+        // }
+
+        // }
+
+        /*-----------TOI UU CODE ---------------*/
         for (int i = 0; i < soDong; i++) {
             int tongTaiSanI = 0;
             for (int j = 0; j < soCot; j++) {
                 tongTaiSanI += accounts[i][j];
             }
-            tongTaiSan[i] = tongTaiSanI;
-            if (tongTaiSan[i] > max) {
-                max = tongTaiSan[i];
+            if (tongTaiSanI > max) {
+                max = tongTaiSanI;
             }
-
         }
 
         return max;
